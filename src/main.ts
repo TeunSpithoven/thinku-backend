5094e16e27eab8ba5ce7bc4806d116787f80734c
Teun Spithoven <teun.spithoven@gmail.com>
2022-11-12 19:38:52 +0100

    increase request size limit to 1mb

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { urlencoded, json } from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
+
   const options = {
     origin: 'http://localhost:8080',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
@@ -12,6 +15,9 @@ async function bootstrap() {
     optionsSuccessStatus: 204,
     credentials: true,
   };
+
+  app.use(json({ limit: '1mb' }));
+  app.use(urlencoded({ extended: true, limit: '1mb' }));
 
   app.enableCors(options);
   await app.listen(3000);
