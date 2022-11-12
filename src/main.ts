63b2d369c850cdd019e2f5d21976e0076a815a02
Teun Spithoven <teun.spithoven@gmail.com>
2022-11-12 19:08:10 +0100

    add cors options

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,15 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
+  const options = {
+    origin: 'http://localhost:8080',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+    credentials: true,
+  };
+
+  app.enableCors(options);
   await app.listen(3000);
 }
 bootstrap();
